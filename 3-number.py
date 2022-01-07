b9667d3d354a6775860532012dab78c0c90c97de
Tubagus Maulana Aghni <58358913+maulanaaghnii@users.noreply.github.com>
2022-01-07 20:22:10 +0700

    number

---
+++ 3-number.py
@@ -0,0 +1,26 @@
+#NUMBER
+
+# Di python tidak hanya ada TEKS , namun juga ada NUMBER, perbedaannya adalah 
+
+print("5") # ini adalah TEKS 
+
+print(5) # ini adalah NUMBER
+
+# Terdapat beberapa jenis number di Python
+# Diantaranya yang paling populer adalah Integer dan Float
+
+# Integer
+print("Ini adalah Integer : ")
+print(1)
+print(2)
+print(3)
+print(100)
+print(1000)
+print(1000000)
+
+
+# Float
+print("Ini adalah Float : ")
+print(1.5)
+print(3.14)
+print(5.0)
