fcd6230ca555cf88c86620a3486467dba5c0207b
Tubagus Maulana Aghni <58358913+maulanaaghnii@users.noreply.github.com>
2022-01-08 14:04:00 +0700

    Math

---
+++ 4-math.py
@@ -0,0 +1,33 @@
+#OPERASI SEDERHANA
+
+# Operasi matematika dari yang sederhana sampai lanjutan dapat dilakukan di bahasa pemrograman Python. Contoh sederhana dari operasi matematika adalah operasi matematika dasar, meliputi operasi tambah, kurang, bagi, kali, dan sisa bagi.
+# 
+# Masing-masing operasi memiliki lambang yang berbeda, 
+# Operasi Tambah dilambangkan       +
+# Operasi Kurang dilambangkan       -
+# Operasi Bagi dilambangkan         /
+# Operasi Kali dilambangkan         *
+# Operasi Sisa Bagi dilambangkan    %  
+
+# Contoh : 
+
+# TAMBAH
+hasil_A =5 + 5
+print(hasil_A)
+
+# KURANG
+hasil_B= 7-5
+print(hasil_B)
+
+# BAGI
+hasil_C = 10/2
+print(hasil_C)
+
+# KALI 
+hasil_D = 5*2
+print(hasil_D)
+
+# SISA BAGI
+hasil_E = 10 % 3
+print(hasil_E)
+
