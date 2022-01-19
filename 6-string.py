fc79578572c5eb420d7589296ec207cf4789264d
Tubagus Maulana Aghni <58358913+maulanaaghnii@users.noreply.github.com>
2022-01-19 14:45:56 +0700

    String

---
+++ 6-string.py
@@ -0,0 +1,23 @@
+#String
+
+
+# String adalah jenis yang paling populer di bahasa pemrograman. Kita bisa membuatnya hanya dengan melampirkan karakter dalam tanda kutip. Python memperlakukan tanda kutip tunggal sama dengan tanda kutip ganda. Membuat string semudah memberi nilai pada sebuah variabel.
+
+# Dibawah ini adalah contoh sederhana dari sebuah string pada bahasa pemrograman Python.
+
+print("Nama Saya Budi")
+
+
+# Memasukan String didalam variable 
+nama = "Budi Doremi"
+nama_depan = "Budi"
+nama_belakang = "Doremi"
+nama_lengkap = nama_depan + nama_belakang
+
+print (nama)
+print(nama_lengkap)
+
+
+# Jika ingin menggunakan spasi
+nama_lengkap = nama_depan + " " + nama_belakang
+print(nama_lengkap)
